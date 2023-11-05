1f851fc4fa4a6ff09b526220bb3ae08bd225eb8d
ADWAITH-553 <114125657+ADWAITH-553@users.noreply.github.com>
2023-11-05 11:20:08 +0530

    REGISTER AND LOGIN PAGE

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,30 @@
 import React from 'react'
-
+import  { useState } from 'react'
 export default function Login() {
+  const[values,setValues]=useState({
+    username:"",
+    email:"",
+    password:""
+  })
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    console.log(values.username)
+    console.log(values.password)
+  }
   return (
-    <div><h1>Login</h1></div>
+    <div className='login'>
+      <form onSubmit={handleSubmit}>
+    <h1>LOGIN</h1>
+      <label>Username</label>
+      <input placeholder="username" type="text" value={values.username} onChange={e=>setValues({...values,username:e.target.value})}/>
+
+      <br />
+      
+      <label>Password</label>
+      <input placeholder="password" type="password" value={values.password} onChange={e=>setValues({...values,password:e.target.value})}/>
+      <br />
+      <button>Submit</button>
+    </form>
+    </div>
   )
 }
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,7 +1,38 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import './Register.css';
 export default function Register() {
+  // const [username,setUsername]=useState(""
+  const[values,setValues]=useState({
+    username:"",
+    email:"",
+    password:""
+  })
+ 
+  
+  
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    console.log(values.username)
+    console.log(values.email)
+    console.log(values.password)
+  }
   return (
-    <div><h1>REGISTER</h1></div>
+    <div className='register'>
+      
+    <form onSubmit={handleSubmit}>
+    <h1>REGISTER</h1>
+      <label>Username</label>
+      <input placeholder="Username" value={values.username} onChange={e=>setValues({...values,username:e.target.value})}/>
+
+      <br />
+      <label>E-mail</label>
+      <input placeholder="Email" value={values.email} onChange={e=>setValues({...values,email:e.target.value})}/>
+      <br />
+      <label>Password</label>
+      <input placeholder="password" type="password" value={values.password} onChange={e=>setValues({...values,password:e.target.value})}/>
+      <br />
+      <button>Submit</button>
+    </form>
+    </div>
   )
 }
