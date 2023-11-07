f616170955dfd9f39316a0603a1a5c252117bc50
ADWAITH-553 <114125657+ADWAITH-553@users.noreply.github.com>
2023-11-07 22:36:05 +0530

    authentication handling in frontend.

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,20 +1,26 @@
 import React, { useState } from 'react'
 import './Register.css';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 export default function Register() {
   // const [username,setUsername]=useState(""
   const[values,setValues]=useState({
     username:"",
-    email:"",
     password:""
   })
  
   
-  
+  const navigate=useNavigate();
   const handleSubmit=(e)=>{
     e.preventDefault();
-    console.log(values.username)
-    console.log(values.email)
-    console.log(values.password)
+    try{
+     // axios.post('',{username:values.username,password:values.password})
+      navigate("/login");
+    }
+    catch(err){
+      console.log(err);
+    }
+    
   }
   return (
     <div className='register'>
@@ -24,9 +30,6 @@ export default function Register() {
       <label>Username</label>
       <input placeholder="Username" value={values.username} onChange={e=>setValues({...values,username:e.target.value})}/>
 
-      <br />
-      <label>E-mail</label>
-      <input placeholder="Email" value={values.email} onChange={e=>setValues({...values,email:e.target.value})}/>
       <br />
       <label>Password</label>
       <input placeholder="password" type="password" value={values.password} onChange={e=>setValues({...values,password:e.target.value})}/>
